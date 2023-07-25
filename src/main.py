25d721d913638797043666c1ae45fceb521e60e7
Mr.peko <jimmy3537@gmail.com>
2023-07-25 11:06:53 +0800

    delete main

--- src/main.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-# options = webdriver.ChromeOptions()
-# options.binary_location = "/Users/tiboogi/Downloads/Google Chrome.app/Contents/MacOS/Google Chrome"
-# chrome_driver_binary = "/path/to/chromedriver"  # Replace with the correct path to chromedriver
-driver = webdriver.Chrome()
-driver.get('https://www.gamer.com.tw/index2.php')
